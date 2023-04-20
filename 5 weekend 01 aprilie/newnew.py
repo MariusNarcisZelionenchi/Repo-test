ef1115dc64847fc1a0a8ddce870d2f1f777633e1
Marius Narcis Zelionenchi <123659805+MariusNarcisZelionenchi@users.noreply.github.com>
2023-04-20 23:18:59 +0300

    Delete 5 weekend 01 aprilie directory

--- 5 weekend 01 aprilie/newnew.py
+++
@@ -1,7 +0,0 @@
-import random
-nr = random.randint(1, 18)
-print(nr)
-agenti = ('bantas', 'bodea', 'bubu', 'dascaluC', 'dascaluD', 'dimitriu', 'florea', 'hriscu', 'iacobescu', 'lupu',
-          'lutcanu', 'mihorianu', 'paduraru', 'rosu', 'scurtu', 'toderica', 'vieriu')
-agent = random.choice(agenti)
-print(agent)
